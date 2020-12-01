32eaf018b89aafcffee3e6990aa2b4d18019b3a1
lacarp99 <lacarp99@gmail.com>
2020-12-01 05:36:27 +0000

    Created file and added basic beginning.

---
+++ main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+class Main {
+  public static void main(String[] args) {
+		int i = 0;
+		while (i < 1) {
+			Console console = System.console();
+			String input = console.readLine("Enter anything: ");
+
+			if (!input.equals("This is the way")) {
+    		System.out.println("I don't know: " + input);				
+			} else {
+				System.out.println("This is the way");
+			}
+
+		}
+
+  }
+}
